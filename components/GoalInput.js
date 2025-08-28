9b2c02abe9cdee8e26ec088731d02a8ead85e957
leodder <a0919989772@gmail.com>
2025-08-28 19:14:44 +0900

    fix: 將statement改寫成expression

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,24 +1,25 @@
 import { StyleSheet, View, Button, TextInput } from "react-native";
-import { useState } from "react"; 
-function GoalInput(props) {
+import { useState } from "react";
+function GoalInput({ onAddGoal }) {
   const [inputValue, setInputValue] = useState("");
   const textInputHandler = (value) => {
     setInputValue(value);
   };
-  function addGoalHanlder () {
-    props.onAddGoal(inputValue);
-    setInputValue('')
-  }
-  return(
-  <View style={styles.inputContainer}>
-    <TextInput
-          style={styles.textInput}
-          placeholder="Your Course Goal!"
-          onChangeText={textInputHandler}
-        />
-    <Button title="Add Goal" onPress={addGoalHanlder} />
-  </View>
-)}
+  const addGoalHanlder = () => {
+    onAddGoal(inputValue);
+    setInputValue("");
+  };
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Your Course Goal!"
+        onChangeText={textInputHandler}
+      />
+      <Button title="Add Goal" onPress={addGoalHanlder} />
+    </View>
+  );
+}
 export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
